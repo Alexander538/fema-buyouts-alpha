fde2a10c49b9633717b98ffee025d68a54da251f
Alexander538 <alexanderferro19@gmail.com>
2022-02-28 12:29:12 -0500

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import Home from './routes/home';
+import MyMap from './routes/usmap';
+import Counties from './routes/counties';
+import County from './routes/county';
+import NoMatch from './components/NoMatch';
+
+
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <h1>Welcome to adaptionapps.com</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="usmap" element={<MyMap />} />
+          <Route path="counties" element={<Counties />}>
+            <Route path=':countyId' element={<County />} />
+          </Route> 
+          
+        
+
+         
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+
+
     </div>
   );
 }
---
+++ src/routes/counties.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Counties() {
+  return (
+    <div>Counties</div>
+  )
+}
+
+export default Counties
